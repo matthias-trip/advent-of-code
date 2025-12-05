335c8d9b6ec4786dc498d407e581bc93d1812068
Matthias Trip <matthias.trip@jamf.com>
2025-12-05 21:45:07 +0100

    Day 2 part 2

--- Sources/2025/Day02_2025.swift
+++ Sources/2025/Day02_2025.swift
@@ -4,13 +4,11 @@ struct Day02_2025: AdventChallenge {
     static var year: Int { 2025 }
     
     var data: String
+    let ranges: [Int]
 
     init(data: String) {
         self.data = data
-    }
-    
-    func part1() async throws -> Any {
-        let result = self.data
+        self.ranges = self.data
             .split(separator: ",")
             .compactMap { range in
                 let ranges = range
@@ -23,17 +21,30 @@ struct Day02_2025: AdventChallenge {
                 return Array(ranges[0]...ranges[1])
             }
             .reduce(into: Array<Int>()) { $0.append(contentsOf: $1) }
-            .filter {
-                let str = String($0).evenlyChunked(in: 2)
-                guard let first = str.first, let last = str.last else {
-                    return false
-                }
-                
-                return first.elementsEqual(last)
+    }
+    
+    func part1() async throws -> Any {
+        return self.ranges.filter {
+            let chunks = String($0).evenlyChunked(in: 2)
+            guard let first = chunks.first, let last = chunks.last else {
+                return false
             }
-            .reduce(0, +)
-        
-        return result
+            
+            return first.elementsEqual(last)
+        }
+        .reduce(0, +)
     }
 
+    func part2() async throws -> Any {
+        return self.ranges.filter {
+            let str = String($0)
+            let range = 1..<str.count
+            
+            return range.contains {
+                let chunks = str.chunks(ofCount: $0)
+                return Set(chunks).count == 1
+            }
+        }
+        .reduce(0, +)
+    }
 }
