1879502d63431660d87049df25cbad208e779757
Matthias Trip <matthias.trip@jamf.com>
2025-12-07 20:50:56 +0100

    Refactor day 3, implement part 2

--- Sources/2025/Day03_2025.swift
+++ Sources/2025/Day03_2025.swift
@@ -10,15 +10,34 @@ struct Day03_2025: AdventChallenge {
         self.data = data
         self.batteries = self.data
             .components(separatedBy: .newlines)
-            .map {$0.compactMap(\.wholeNumberValue) }
+            .map { $0.compactMap(\.wholeNumberValue) }
+            .filter { !$0.isEmpty }
+    }
+    
+    func largestJoltage(from batteries: [Int], digits: Int) -> Int {
+        (0..<digits)
+            .reversed()
+            .reduce((currentIndex: 0, value: 0)) { state, left in
+                let (currentIndex, value) = state
+                let (ix, v) = batteries.enumerated()
+                    .dropFirst(currentIndex)
+                    .dropLast(left)
+                    .max(by: { $0.element < $1.element })!
+                
+                return (ix + 1, value * 10 + v)
+            }
+            .value
     }
     
     func part1() async throws -> Any {
-        return self.batteries
-            .compactMap {
-                $0.combinations (ofCount: 2)
-                    .map {$0[0] * 10 + $0[1] }
-                    .max ()
-        }.reduce(0, +)
+        self.batteries
+            .map { self.largestJoltage(from: $0, digits: 2) }
+            .reduce(0, +)
+    }
+    
+    func part2() async throws -> Any {
+        self.batteries
+            .map { self.largestJoltage(from: $0, digits: 12) }
+            .reduce(0, +)
     }
 }
