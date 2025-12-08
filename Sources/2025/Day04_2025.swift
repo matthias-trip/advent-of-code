20ff67633991c04395047f7b205942f874834b41
Matthias Trip <matthias.trip@jamf.com>
2025-12-08 11:08:35 +0100

    Day 4 part 2

--- Sources/2025/Day04_2025.swift
+++ Sources/2025/Day04_2025.swift
@@ -18,16 +18,51 @@ struct Day04_2025: AdventChallenge {
     }
     
     func part1() async throws -> Any {
-        let paperPositions = self.grid.allPositions()
-            .filter { self.grid.value(at: $0) == Self.paperRoll }
-        
+        let paperPositions = self.grid.allPositionsWithValue(Self.paperRoll)
         return paperPositions
-            .filter {
-                return self.grid.value(at: $0) == Self.paperRoll
-            }
-            .filter {
-                return self.grid.adjacentValues(of: $0).count(where: { $0 == Self.paperRoll }) < 4
-            }
+            .filter { self.hasPaperRoll(at: $0, on: self.grid) && self.hasReachablePaperRoll(at: $0, on: self.grid) }
             .count
+    }
+    
+    func part2() async throws -> Any {
+        var totalRemoved = 0
+        var removedInCurrentIteration = 0
+        
+        var grid = self.grid
+        
+        while(true) {
+            removedInCurrentIteration = 0
+            let paperPositions = grid.allPositionsWithValue(Self.paperRoll)
+            
+            for position in paperPositions {
+                if self.hasReachablePaperRoll(at: position, on: grid) {
+                    removedInCurrentIteration += 1
+                    totalRemoved += 1
+                    
+                    grid.setValue("x", at: position)
+                }
+            }
+            
+            if removedInCurrentIteration == 0 {
+                break
+            }
+        }
+        
+        
+        return totalRemoved
+    }
+    
+    private func hasPaperRoll(at position: Position, on grid: Grid<Character>) -> Bool {
+        return grid.value(at: position) == Self.paperRoll
+    }
+    
+    private func hasReachablePaperRoll(at position: Position, on grid: Grid<Character>) -> Bool {
+        return grid.adjacentValues(of: position).count(where: { $0 == Self.paperRoll }) < 4
+    }
+}
+
+extension Grid where Value == Character {
+    func allPositionsWithValue(_ value: Character) -> [Position] {
+        return self.allPositions().filter{ self.value(at: $0) == value }
     }
 }
